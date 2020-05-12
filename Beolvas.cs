472efd0bd3d4b4ccc1d2b938562098fa771881dd
grandmastar27 <grandmastar27@gmail.com>
2020-05-12 18:50:17 +0200

    Create Beolvas.cs

---
+++ Beolvas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace lottoGUI
+{
+    class Lotto
+    {
+        public string lottoszamokSor { get; set; }
+        public int Sor { get; set; }
+        public int[,] lottoszamok { get; set; }
+        public Lotto(string lottoszamokSor, int sor, int[,] lottoszamok)
+        {
+            this.lottoszamokSor = lottoszamokSor;
+            this.Sor = sor;
+            this.lottoszamok = lottoszamok;
+        }
+    }
+}
